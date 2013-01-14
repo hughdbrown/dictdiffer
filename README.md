8494c76c3054853770616449cf162bd4400eca59
Hugh Brown <hbrown@wgen.net>
2013-01-14 15:29:19 -0500

    Initial revision

---
+++ README.md
@@ -0,0 +1,10 @@
+=== DictDiffer
+Python class to take the difference of two python dictionaries:
+- stuff added
+- stuff deleted
+- stuff changed
+- stuff unchanged
+
+Originally presented in a StackoverFlow answer:
+http://stackoverflow.com/questions/1165352/fast-comparison-between-two-python-dictionary/1165552#1165552
+
---
+++ dictdiffer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""
+A dictionary difference calculator
+Originally posted as:
+http://stackoverflow.com/questions/1165352/fast-comparison-between-two-python-dictionary/1165552#1165552
+"""
+
+
+class DictDiffer(object):
+    # Calculate the difference between two dictionaries as:
+    # (1) items added
+    # (2) items removed
+    # (3) keys same in both but changed values
+    # (4) keys same in both and unchanged values
+    """
+    >>> a = {'a': 1, 'b': 1, 'c': 0}
+    >>> b = {'a': 1, 'b': 2, 'd': 0}
+    >>> d = DictDiffer(b, a)
+    >>> print "Added:", d.added()
+    Added: set(['d'])
+    >>> print "Removed:", d.removed()
+    Removed: set(['c'])
+    >>> print "Changed:", d.changed()
+    Changed: set(['b'])
+    >>> print "Unchanged:", d.unchanged()
+    Unchanged: set(['a'])    
+    """
+    def __init__(self, current_dict, past_dict):
+        self.current_dict, self.past_dict = current_dict, past_dict
+        self.set_current, self.set_past = set(current_dict.keys()), set(past_dict.keys())
+        self.intersect = self.set_current.intersection(self.set_past)
+    def added(self):
+        return self.set_current - self.intersect 
+    def removed(self):
+        return self.set_past - self.intersect 
+    def changed(self):
+        return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
+    def unchanged(self):
+        return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
+
+
+if __name__ == '__main_':
+    from doctest import testmod
+    testmod()
