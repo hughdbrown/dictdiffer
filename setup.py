8d0c015db098f9c0b0b9e0b0607e702e5375b2ff
Martin Ortbauer <mortbauer@gmail.com>
2013-08-15 14:03:20 +0200

    changed the package structure and added a basic setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup
+
+setup(name='dictdiffer',
+      version='1.0',
+      description='Python class to calculate the difference between two python dictionaries',
+      author='Hugh Brown',
+      url='https://github.com/mortbauer/dictdiffer',
+      packages=['dictdiffer'],
+      )
