a21de1e124fb05f4a4d376b92c6adda37de83a41
Hugh Brown <hughdbrown@yahoo.com>
2018-03-02 14:16:50 -0700

    Add new_or_changed; add docstrings

--- dictdiffer/__init__.py
+++ dictdiffer/__init__.py
@@ -16,21 +16,28 @@ class DictDiffer(object):
     """
     def __init__(self, current_dict, past_dict):
         self.current_dict, self.past_dict = current_dict, past_dict
-        self.current_keys, self.past_keys = [
-            set(d.keys()) for d in (current_dict, past_dict)
-        ]
+        self.current_keys, self.past_keys = set(current_dict.keys()), set(past_dict.keys())
         self.intersect = self.current_keys.intersection(self.past_keys)
 
     def added(self):
+        """ Find keys that have been added """
         return self.current_keys - self.intersect
 
     def removed(self):
+        """ Find keys that have been removed """
         return self.past_keys - self.intersect
 
     def changed(self):
+        """ Find keys that have been changed """
         return set(o for o in self.intersect
                    if self.past_dict[o] != self.current_dict[o])
 
     def unchanged(self):
+        """ Find keys that are unchanged """
         return set(o for o in self.intersect
                    if self.past_dict[o] == self.current_dict[o])
+
+    def new_or_changed(self):
+        """ Find keys that are new or changed """
+        return set(k for k, v in self.current_dict.items()
+                   if k not in self.past_keys or v != self.past_dict[k])
